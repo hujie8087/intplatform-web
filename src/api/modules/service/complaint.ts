ae5e49efa9d07ac8d1ba9a72cbcf0cba841fc6a6
hujie <80776811@qq.com>
2024-12-07 08:05:02 +0800

    feat: 删除投诉建议类型模块

--- src/api/modules/service/complaint.ts
+++ src/api/modules/service/complaint.ts
@@ -36,24 +36,3 @@ export const deleteMoreComplaintMessage = (params: number[]) => {
 export const getComplaintMessageList = (params: Complaint.ReqComplaintMessageParams) => {
   return http.get<Complaint.ResComplaintMessage[][]>(PORT1 + `/other/ComplaintMessage/list`, params);
 };
-
-// // 获取投诉与建议类型列表
-// export const getComplaintTypeList = (params: Complaint.ReqComplaintTypeParams) => {
-//   return http.getRow<Complaint.ResComplaintType>(PORT1 + `/other/ComplaintType/list`, params);
-// };
-// // * 获取投诉与建议类型
-// export const getComplaintTypeById = (id: number) => {
-//   return http.get<Complaint.ResComplaintType>(PORT1 + `/other/ComplaintType/${id}`);
-// };
-// // * 新增投诉与建议类型
-// export const addComplaintType = (params: FormData) => {
-//   return http.post(PORT1 + `/other/ComplaintType`, params);
-// };
-// // * 编辑投诉与建议类型
-// export const editComplaintType = (params: FormData) => {
-//   return http.put(PORT1 + `/other/ComplaintType`, params);
-// };
-// // * 删除投诉与建议类型
-// export const deleteComplaintType = (id: number) => {
-//   return http.delete(PORT1 + `/other/ComplaintType/${id}`);
-// };
