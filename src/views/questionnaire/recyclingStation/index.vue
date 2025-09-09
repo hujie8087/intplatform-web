391d575e9fc7f55067ddfdffe40130fdc9fdc8d2
zwx970624 <630140309@qq.com>
2025-09-09 09:38:58 +0900

    feat: 问卷系统回收站增加恢复和删除功能

--- src/views/questionnaire/recyclingStation/index.vue
+++ src/views/questionnaire/recyclingStation/index.vue
@@ -12,113 +12,24 @@
         :search-col="{ xs: 1, sm: 1, md: 3, lg: 6, xl: 6 }"
       >
         <!-- 表格 header 按钮 -->
-        <!-- <template #tableHeader> -->
-        <!-- <el-button type="primary" v-auth="['system:user:add']" :icon="CirclePlus" @click="addSurvey('新增')">
+        <!-- <template #tableHeader>
+        <el-button type="primary" v-auth="['system:user:add']" :icon="CirclePlus" @click="addSurvey('新增')">
               新建问卷
-            </el-button> -->
-        <!-- <el-button type="primary" v-auth="['system:user:import']" :icon="Upload" plain @click="batchAdd">
-                批量添加用户
-              </el-button>
-              <el-button type="warning" v-auth="['system:user:export']" :icon="Download" plain @click="downloadFile">
-                导出用户数据
-              </el-button> -->
-        <!-- </template> -->
+            </el-button>
+       </template> -->
         <!-- 表格操作 -->
-        <!-- <template #operation="scope"> -->
-        <!-- <el-button type="primary" link :icon="View" @click="openDrawer('查看', scope.row)">查看</el-button> -->
-
-        <!-- <el-tooltip placement="top" effect="dark" content="编辑">
-              <el-button
-                type="warning"
-                link
-                v-if="scope.row.userId !== 1"
-                :icon="EditPen"
-                v-auth="['system:user:edit']"
-                @click="addSurvey('编辑', scope.row.projectKey)"
-              >
-              </el-button>
-            </el-tooltip> -->
-        <!-- <el-tooltip placement="top" effect="dark" content="答卷数据">
-              <el-button
-                type="success"
-                link
-                v-if="scope.row.userId !== 1"
-                :icon="DataLine"
-                v-auth="['system:user:edit']"
-                @click="addSurvey('编辑', scope.row)"
-              >
-              </el-button>
-            </el-tooltip> -->
-        <!-- <el-tooltip placement="top" effect="dark" content="预览问卷">
-              <el-button
-                type="primary"
-                link
-                v-if="scope.row.userId !== 1"
-                :icon="View"
-                v-auth="['system:user:edit']"
-                @click="addSurvey('编辑', scope.row)"
-              >
-              </el-button>
-            </el-tooltip> -->
-        <!-- <el-tooltip placement="top" effect="dark" content="答卷地址">
-              <el-button
-                type="info"
-                class="btn-custom"
-                link
-                v-if="scope.row.userId !== 1"
-                :icon="Share"
-                v-auth="['system:user:edit']"
-                @click="addSurvey('编辑', scope.row)"
-              >
-              </el-button>
-            </el-tooltip> -->
-        <!-- <el-tooltip placement="top" effect="dark" content="复制问卷">
-              <el-button
-                type="info"
-                class="btn-copySurvey"
-                link
-                v-if="scope.row.userId !== 1"
-                :icon="CopyDocument"
-                v-auth="['system:user:edit']"
-                @click="addSurvey('编辑', scope.row)"
-              >
-              </el-button>
-            </el-tooltip> -->
-
-        <!-- <el-tooltip placement="top" effect="dark" content="删除问卷">
-              <el-button
-                type="danger"
-                link
-                :icon="Delete"
-                v-auth="['system:user:remove']"
-                @click="deleteSurvey(scope.row)"
-              ></el-button>
-            </el-tooltip> -->
-        <!-- <el-dropdown v-if="scope.row.userId !== 1" style="display: inline-block; margin-left: 10px; vertical-align: middle">
-                <el-button type="success" link :icon="DArrowRight">更多</el-button>
-                <template #dropdown>
-                  <el-dropdown-menu>
-                    <el-dropdown-item>
-                      <el-button type="primary" link :icon="Refresh" v-auth="['system:user:resetPwd']" @click="resetPass(scope.row)"
-                        >重置密码</el-button
-                      >
-                    </el-dropdown-item>
-                    <el-dropdown-item>
-                      <el-button
-                        type="primary"
-                        link
-                        :icon="CircleCheck"
-                        v-auth="['system:user:edit']"
-                        @click="handleAuthRole(scope.row)"
-                        >分配角色</el-button
-                      >
-                    </el-dropdown-item>
-                    <el-dropdown-item>
-                    </el-dropdown-item>
-                  </el-dropdown-menu>
-                </template>
-              </el-dropdown> -->
-        <!-- </template> -->
+        <template #operation="scope">
+          <el-popconfirm class="box-item" title="确认恢复该问卷" placement="bottom" @confirm="recoverSurver(scope.row)">
+            <template #reference>
+              <el-button type="primary" link :icon="DataLine">恢复</el-button>
+            </template>
+          </el-popconfirm>
+          <el-popconfirm class="box-item" title="确认删除该问卷" placement="bottom" @confirm="deleteSurvey(scope.row)">
+            <template #reference>
+              <el-button type="danger" link :icon="Delete" v-auth="['system:user:edit']"> 删除 </el-button>
+            </template>
+          </el-popconfirm>
+        </template>
       </ProTable>
     </div>
   </div>
@@ -126,11 +37,12 @@
 
 <script setup lang="tsx" name="recyclingStation">
 import { ref, reactive, onMounted } from "vue";
-// deleteProject, undeleteQuestionnaire
 import ProTable from "@/components/ProTable/index.vue";
-import { getProjectList } from "@/api/modules/questionnaire/recyclingStation";
+import { Delete, DataLine } from "@element-plus/icons-vue";
+import { getProjectList, undeleteQuestionnaire, deleteProject } from "@/api/modules/questionnaire/recyclingStation";
 import { RecyclingStation } from "@/api/interface/questionnaire/recyclingStation";
 import { surveyType } from "@/utils/questionnaire";
+import { ElMessage } from "element-plus";
 const proTable = ref();
 const treeFilterValues = reactive({
   pageNum: 1,
@@ -173,8 +85,24 @@ const columns = reactive([
       let text = status == 1 ? "未发布" : status == 2 ? "收集中" : "停止发布";
       return <span>{<el-tag type={status === 1 ? "primary" : status === 2 ? "success" : "danger"}>{text}</el-tag>}</span>;
     }
-  }
+  },
+  { prop: "operation", label: "操作", fixed: "right" }
 ]);
+const deleteSurvey = async row => {
+  const res = await deleteProject(row.projectKey);
+  if (res.code == 200) {
+    ElMessage.success(`问卷删除${res.msg}`);
+  }
+  proTable.value?.getTableList();
+};
+// 恢复问卷
+const recoverSurver = async row => {
+  const res = await undeleteQuestionnaire(row.projectKey);
+  if (res.code == 200) {
+    ElMessage.success(`问卷恢复${res.msg}`);
+  }
+  proTable.value?.getTableList();
+};
 onMounted(async () => {
   // await getProjectList(treeFilterValues);
 });
