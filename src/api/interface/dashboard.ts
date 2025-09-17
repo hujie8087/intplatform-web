1c7dde6ed4500a8882a499273ba9c82f7fdbf90b
zwx970624 <630140309@qq.com>
2025-09-18 08:15:41 +0900

    feat: 增加大屏接口api

--- src/api/interface/dashboard.ts
+++ src/api/interface/dashboard.ts
@@ -18,4 +18,99 @@ export namespace DataVisualize {
     num: number;
     type: string;
   }
+  export interface Personnel {}
+  export interface RepairStatistics {
+    totalCount: number;
+    waitReWorkCount: number;
+    waitingCount: number;
+    finishCount: number;
+    reWorkCount: number;
+    endCount: number;
+    finishCountIn24H: number;
+    finishRateIn24H: number;
+  }
+  export interface CheckHiddenDanger {
+    totalCount: number;
+    waitCount: number;
+    processingCount: number;
+    processedCount: number;
+  }
+  export interface MealService {
+    orderCount: number;
+    foodCount: number;
+    deliveryCount: number;
+    deliveredCount: number;
+  }
+  export interface SiteInformation {
+    msg: string;
+    code: number;
+    data: SiteInformationList[];
+  }
+  export interface CarLine {
+    msg: string;
+    code: number;
+    data: CarListItem[];
+  }
+  export interface CarMessge {
+    msg: string;
+    code: number;
+    data: {
+      deliveredCount: number;
+      deliveryCount: number;
+      foodCount: number;
+      orderCount: number;
+    };
+  }
+}
+
+interface ChartSeries {
+  name: string;
+  type: string;
+  data: any[];
+  symbol: string;
+  areaStyle: any;
+  smooth: boolean;
+  symbolSize: number;
+  showSymbol: boolean;
+  itemStyle: any;
+}
+export interface chartOptionType {
+  xData: (number | string)[];
+  seriesDat: ChartSeries[];
+}
+
+interface YearDataItem {
+  month: number; // 月份 (1-12)
+  totalCount: number; // 总用户数
+  loginCount: number; // 登录数
+  registerCount: number; // 注册数
+}
+interface SiteInformationList {
+  fsId: number;
+  fsAddressId: string;
+  latitude: number | null;
+  longitude: number | null;
+  nationType: string | null;
+  orderCount: number;
+  goodsCount: number;
+  remainingGoodsCount: number;
+  receivedGoodsCount: number;
+}
+type Coordinate = [number, number];
+interface CarListItem {
+  coordinatesList: string | null;
+  fcId: number;
+  fcName: string;
+  line: Coordinate[]; // 坐标数组（包含多个 [number, number]）
+}
+export interface ResponseType {
+  msg: string;
+  code: number | string;
+  data: {
+    allCount: number | string;
+    onlineCount: number | string;
+    todayLoginCount: number | string;
+    todayRegisterCount: number | string;
+    yearData: YearDataItem[];
+  };
 }
--- src/api/modules/dashboard.ts
+++ src/api/modules/dashboard.ts
@@ -17,3 +17,36 @@ export const getCateringScreenData = (params: { beginTime?: string; endTime?: st
     ...params
   });
 };
+
+// 人员统计信息
+export const getPersonnelStatistics = params => {
+  return http.get<DataVisualize.Personnel>(PORT1 + `system/statistics/data`, params, { loading: false });
+};
+
+// 维修统计信息
+export const getRepairStatistics = (params: { dateType?: string; ancestors?: string }) => {
+  return http.get<DataVisualize.RepairStatistics>(PORT1 + `maintenance/statistics/repair`, params, { loading: false });
+};
+
+// 排查隐患
+export const getCheckHiddenDanger = (params: { dateType?: string; ancestors?: string }) => {
+  return http.get<DataVisualize.CheckHiddenDanger>(PORT1 + `maintenance/statistics/hiddendanger`, params, { loading: false });
+};
+// 报餐送餐-汇总信息
+export const getMealService = (params: { dateType?: string; ancestors?: string }) => {
+  return http.get<DataVisualize.MealService>(PORT1 + `order/statistics/count`, params, { loading: false });
+};
+// 报餐送餐-站点信息
+export const getSiteInformation = (params: { date?: string; foodName?: string }) => {
+  return http.get<DataVisualize.SiteInformation>(PORT1 + `/order/statistics/site`, params, { loading: false });
+};
+
+// 报餐送餐-货车轨迹
+export const getCarLine = (params: { date?: string; foodName?: string; fcName?: string }) => {
+  return http.get<DataVisualize.CarLine>(PORT1 + `/order/statistics/car/line`, params, { loading: false });
+};
+
+// 报餐送餐-指定车辆数据
+export const getCarMessge = (params: { date?: string; foodName?: string; fcName?: string }) => {
+  return http.get<DataVisualize.CarMessge>(PORT1 + `/order/statistics/car/data`, params, { loading: false });
+};
