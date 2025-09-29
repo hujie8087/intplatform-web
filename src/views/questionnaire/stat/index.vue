25d2915a725342706a6a3584e560a200c5d7c281
zwx970624 <630140309@qq.com>
2025-09-29 16:42:40 +0900

    fix: 统计页面增加权限字段

--- src/views/questionnaire/stat/index.vue
+++ src/views/questionnaire/stat/index.vue
@@ -2,16 +2,16 @@
   <div class="stat-page">
     <div class="content-card">
       <el-tabs type="border-card" v-model="activeName" class="demo-tabs tong-ji-page" @tab-click="handleClick">
-        <el-tab-pane label="列表视图" name="first">
+        <el-tab-pane label="列表视图" name="first" v-auth="['project:answer:list']">
           <listView v-if="activeName === 'first'" />
         </el-tab-pane>
-        <el-tab-pane label="统计视图" name="second">
+        <el-tab-pane label="统计视图" name="second" v-auth="['survey:stats:stats']">
           <statView v-if="activeName === 'second'" />
         </el-tab-pane>
-        <el-tab-pane label="数据分析" name="third">
+        <el-tab-pane label="数据分析" name="third" v-auth="['survey:stats:analysis']">
           <dataAnalyze v-if="activeName === 'third'" />
         </el-tab-pane>
-        <el-tab-pane label="投票排名" name="fourth">
+        <el-tab-pane label="投票排名" name="fourth" v-auth="['survey:stats:rank']">
           <voteRank v-if="activeName === 'fourth'" />
         </el-tab-pane>
       </el-tabs>
